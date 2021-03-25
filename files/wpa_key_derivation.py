0a2ca66e33865bacdf287a8afeb33a6da9a4cbfe
Robin Müller <33422394+AziXus@users.noreply.github.com>
2021-03-25 13:53:48 +0100

    Working on lab

--- files/wpa_key_derivation.py
+++ files/wpa_key_derivation.py
@@ -18,7 +18,9 @@ __status__ 		= "Prototype"
 
 from scapy.all import *
 from binascii import a2b_hex, b2a_hex
-from pbkdf2_math import pbkdf2_hex
+#from pbkdf2_math import pbkdf2_hex
+from scapy.layers.eap import *
+load_contrib("wpa_eapol")
 from pbkdf2 import *
 from numpy import array_split
 from numpy import array
@@ -43,13 +45,16 @@ wpa=rdpcap("wpa_handshake.cap")
 # Important parameters for key derivation - most of them can be obtained from the pcap file
 passPhrase  = "actuelle"
 A           = "Pairwise key expansion" #this string is used in the pseudo-random function
-ssid        = "SWI"
-APmac       = a2b_hex("cebcc8fdcab7")
-Clientmac   = a2b_hex("0013efd015bd")
+print(wpa[3].addr2)
+ssid        = wpa[3].info.decode('ascii')
+APmac       = a2b_hex(wpa[3].addr1.replace(':', ''))
+Clientmac   = a2b_hex(wpa[3].addr2.replace(':', ''))
+
+print()
 
 # Authenticator and Supplicant Nonces
-ANonce      = a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
-SNonce      = a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
+ANonce      = a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91") # 6
+SNonce      = wpa[6].getlayer(WPA_key).nonce # 7
 
 # This is the MIC contained in the 4th frame of the 4-way handshake
 # When attacking WPA, we would compare it to our own MIC calculated using passphrases from a dictionary
